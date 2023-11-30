8f3d9805eef040ad4c486426637c38e1e68053d0
Zakishan200 <94130813+Zakishan200@users.noreply.github.com>
2023-11-30 19:31:19 +0100

    Ajout de getGrid et setCase (world.py)

--- world.py
+++ world.py
@@ -22,4 +22,16 @@ class World():
     def __init__(self, x_size: int, y_size: int) -> None:
         x, y = np.arange(0, x_size, 1), np.arange(0, y_size, 1)
         grid_x, grid_y = np.meshgrid(x, y)
-        grid = np.zeros_like(grid_x, dtype=Case)  #Manipuler grid avec grid[(x, y)]
+        self.grid = np.zeros_like(grid_x, dtype=Case)  #Manipuler grid avec grid[(x, y)]
+
+    @property
+    def getGrid(self):
+        return self.grid
+    
+    def setCase(self, x: int, y: int, type: str):
+        if type == "FOREST":
+            self.grid[(x,y)] = CaseForest()
+
+
+        
+
