9fdc5eedfba9c48750f5c9292ebca6bf09ffe1cb
Zakishan200 <z.sbrpro@gmail.com>
2023-12-01 23:35:37 +0100

    ajout de config et début d'affichage

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import numpy as np
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtGui import QPixmap
+import json
 
 class mainWindow(QWidget):
     def __init__(self, width: int, height: int) -> None:
         super().__init__()
+
+        with open('config.json') as f:
+            self.config = json.load(f)
         self.initUI(width, height)
 
     def initUI(self, width, height):
@@ -17,34 +21,30 @@ class mainWindow(QWidget):
         self.setWindowTitle("2D_simulation")
 
         #Initialisation des pixmap
-        self.forest_pixmap = QPixmap('forest_image.jpg')
-        self.mountain_pixmap = QPixmap('mountain_image.jpg')
+        self.forest_pixmap = QPixmap(self.config['TilesPath']['path_forest'])
+        self.mountain_pixmap = QPixmap(self.config['TilesPath']['path_mountain'])
 
-        self.label_image = QLabel(self)
-        self.label_image.setGeometry(0, 0, 50, 50)
 
-        image_path = 'forest_image.jpg'
-        self.pixmap = QPixmap(image_path)
-        self.pixmop = QPixmap('mountain_image.jpg')
-        self.label_image.setPixmap(self.pixmap)
-        self.label_image.setPixmap(self.mountain_pixmap)
+    
+    def showWorld(self, world: wd.World):
+        grid = world.getGrid
+        i_size, j_size = grid.shape
+        for i in range(i_size):
+            for j in range(j_size):
+                case = grid[(i, j)]
+                print(case.case_type)
+                label_image = QLabel(self)
+                label_image.setGeometry(i*50, j*50, 50, 50)
+                label_image.setPixmap(self.forest_pixmap)
 
-    def afficher_image_aux_coordonnees(self, image_path, x, y, largeur, hauteur):
-        # Charger l'image depuis un fichier PNG
-        pixmap = QPixmap(image_path)
-
-        # Redimensionner l'image si nécessaire
-        pixmap = pixmap.scaled(largeur, hauteur)
-
-        # Afficher l'image dans le QLabel aux coordonnées spécifiées
-        self.label_image.setGeometry(x, y, largeur, hauteur)
-        self.label_image.setPixmap(pixmap)
-        self.label_image.update()
-
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = mainWindow(1280, 720)
-    #window.afficher_image_aux_coordonnees("forest_image.png", 50, 50, 50, 50)
     window.show()
+    world = wd.World(10, 10)
+    world.create_world()
+    window.showWorld(world)
+
     
     sys.exit(app.exec_())
--- world.py
+++ world.py
@@ -23,11 +23,18 @@ class World():
         x, y = np.arange(0, x_size, 1), np.arange(0, y_size, 1)
         grid_x, grid_y = np.meshgrid(x, y)
         self.grid = np.zeros_like(grid_x, dtype=Case)  #Manipuler grid avec grid[(x, y)]
+        self.i_size, self.j_size = x_size, y_size
 
     @property
     def getGrid(self):
         return self.grid
     
+    def create_world(self):
+        for i in range(self.i_size):
+            for j in range(self.j_size):
+                self.setCase(i, j, "FOREST")
+            
+
     def setCase(self, x: int, y: int, type: str):
         if type == "FOREST":
             self.grid[(x,y)] = CaseForest()
