9d87a5b21764c226eba3b6f0cd4d99ee20707368
Xujason <1xujason@gmail.com>
2024-02-29 22:29:59 +0100

    solving bug about land and adding mountain random generated , the display need to be checked because my pc can not show all the window and it crash when clicking on it so
    authered-by xu zexuan

--- world.py
+++ world.py
@@ -3,7 +3,64 @@ from world_cases import *
 from world_entities import *
 import random
 
+class Mountain_builder:
+    type = None
+    high = None
+    i = None
+    j = None
 
+    def build_mountain(self, i, j, matrix):
+        self.set_high(random.randint(1,5))
+        self.set_mountain_type(random.randint(1,2))
+        self.i = i+1-self.high
+        self.j = j
+        if self.type == "A":
+            self.build_type_A(matrix)
+        elif self.type == "B":
+            self.build_type_B(matrix)
+
+    def build_type_A(self, matrix):
+        for i in range(self.high):
+            matrix[self.i+i][self.j] = "L"
+            for j in range(i):
+                matrix[self.i+i][self.j+j] = "L"
+                matrix[self.i+i][self.j-j] = "L"
+
+
+    def build_type_B(self, matrix):
+        if self.high == 1:
+            matrix[self.i][self.j] = "L"
+            return
+        else:
+            left = random.randint(1, self.high - 1)
+            right = random.randint(1, self.high - 1)
+            matrix[self.i][self.j] = "L"
+            for i in range(1, self.high):
+                matrix[self.i + i][self.j] = "L"
+                if (self.high - i) == left and left:
+                    matrix[self.i + i][self.j - 1] = "L"
+                    left -= 1
+                if (self.high - i) == right and right:
+                    matrix[self.i + i][self.j + 1] = "L"
+                    right -=1
+
+
+
+    def build_type_C(self, matrix):
+        pass
+    def set_mountain_type(self, high: int):
+        if high == 1:
+            self.type = "A"
+        elif high == 2:
+            self.type = "B"
+        elif high == 3:
+            self.type = "C"
+        else:
+            print("error in random parameter given is not accepted")
+            exit(1)
+
+    def set_high(self, high: int):
+        self.high = high
 def generate_entities(entities_file):
     used_position = set()
     with open("World data/entities_setting.txt", 'r') as file, open(entities_file, 'w') as entities:
@@ -98,36 +155,50 @@ class World:
 
         line_values = ['.'] * sky_layers + ['S'] * surfacesea_layers + ['M'] * sea_layers + ['D'] * deepsea_layers
         world_matrix = [[value] * self.j_size for value in line_values]
-        self.generate_random_floor(world_matrix)
+        self.generate_random_floor(world_matrix, sky_layers)
         for i in range(self.i_size):
             for j in range(self.j_size):
                 self.set_case(j, i, world_matrix[i][j])
         self.create_entities(foreground)
     # RANDOM GENERATE FLOOR
-    def generate_random_floor(self, world_matrix):
-        # longueur des cotes donc partie de terre a la surface de la mer jusqu au point minimal que
+    def generate_random_floor(self, world_matrix, skylayers):
+        # longueur des terrains de terre a la surface de la mer
         cote_left_length = random.randint(3, 10)
-        cote_down_height = random.randint(cote_left_length, cote_left_length+10)
-        height_floor = random.randint(5, 15)
+        # longueur de la pente du cote gauche jusqu a la fin de la pente
+        cote_down_height = random.randint(cote_left_length+3, cote_left_length+10)
+        # hauteur de la couche sol en dessous de la mer
+        height_floor = random.randint(2, 5)
+        #  |                                  |here sky
+        #  |L--------------------------------L| here land part on the surface of the sea which cote_left_length = 1
+        #  |LLLL--------------------------LLLL| here is the length of the end of the pente which cote_down_height = 4
+        #  |LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL|
+        #  |LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL| here is the height of the floor under the sea which is height_floor = 2
         # make horizontal floor
         for i in range(self.i_size-1,self.i_size-1-height_floor, -1):
             world_matrix[i] = ["L"] * self.j_size
         # make vertical left floor
         for i in range(self.i_size):
-            if world_matrix[i][0] != "L":
+            if world_matrix[i][0] != "L" and world_matrix[i][0] != ".":
                 for j in range(0, cote_left_length, 1):
                     world_matrix[i][j] = "L"
         # make vertical right floor
         for i in range(self.i_size):
-            if world_matrix[i][self.j_size-1] != "L":
+            if world_matrix[i][self.j_size-1] != "L" and world_matrix[i][0] != ".":
                 for j in range(self.j_size-1, self.j_size-1 - cote_left_length, -1):
                     world_matrix[i][j] = "L"
         # creation de la pente
-        for i in range(0, self.i_size-1-height_floor):
-            point = self.interpolate_points(self.i_size-1-i, cote_left_length, self.i_size - 1, cote_down_height, height_floor)
+        for i in range(skylayers, self.i_size-height_floor):
+            point = self.interpolate_points(self.i_size-1-i, cote_left_length, self.i_size - 1-skylayers, cote_down_height, height_floor)
             for j in range(1, int(point) - cote_left_length+1):
                 world_matrix[i][cote_left_length-1+j] = "L"
                 world_matrix[i][self.j_size - cote_left_length -j] = "L"
+        for j in range(cote_down_height+4, self.j_size-cote_down_height-5, 7):
+            self.add_mountain(self.i_size-1-height_floor, j, world_matrix)
+
+    # create a mountain at a location
+    def add_mountain(self, i, j, matrix):
+        builder = Mountain_builder()
+        builder.build_mountain(i, j, matrix)
 
     def interpolate_points(self, y, x1, y1, x2, y2):
         """
